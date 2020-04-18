ce3dbcdafaf9d242b9888a68864c8696e476e022
Viacheslav Burlaka <viacheslav.burlaka@uvoteam.com>
2020-04-18 23:37:03 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/scss/index.scss';
+import './styles/less/index.less';
+import App from "./containers/App.jsx";
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,169 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all'
+    }
+  };
+
+  //
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCssAssetWebpackPlugin(),
+      new TerserWebpackPlugin()
+    ]
+  }
+
+  return config
+};
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`;
+
+const cssLoaders = extra => {
+  const loaders = [
+    {
+      loader: MiniCssExtractPlugin.loader,
+      options: {
+        hmr: isDev,
+        reloadAll: true
+      },
+    },
+    'css-loader'
+  ];
+
+  if (extra) {
+    loaders.push(extra)
+  }
+
+  return loaders
+};
+
+const babelOptions = preset => {
+  const opts = {
+    presets: [
+      '@babel/preset-env'
+    ],
+    plugins: [
+      '@babel/plugin-proposal-class-properties'
+    ]
+  };
+
+  if (preset) {
+    opts.presets.push(preset)
+  }
+
+  return opts
+};
+
+
+const jsLoaders = () => {
+  const loaders = [{
+    loader: 'babel-loader',
+    options: babelOptions('@babel/preset-react')
+  }];
+
+  if (isDev) {
+    loaders.push('eslint-loader')
+  }
+
+  return loaders
+};
+
+const plugins = () => {
+  return [
+    new HTMLWebpackPlugin({
+      template: './index.html',
+      minify: {
+        collapseWhitespace: isProd
+      }
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, 'src/favicon.ico'),
+        to: path.resolve(__dirname, 'dist')
+      }
+    ]),
+    new MiniCssExtractPlugin({
+      filename: filename('css')
+    })
+  ]
+};
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: {
+    main: [
+      '@babel/polyfill',
+      './index.js'
+    ],
+  },
+  output: {
+    filename: filename('js'),
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.png'],
+  },
+  optimization: optimization(),
+  devServer: {
+    port: 4200,
+    watchContentBase: true,
+    hot: isDev
+  },
+  devtool: isDev ? 'source-map' : '',
+  plugins: plugins(),
+  module: {
+    rules: [
+      // Loading styles
+      {
+        test: /\.css$/,
+        use: cssLoaders()
+      },
+      {
+        test: /\.less$/,
+        use: cssLoaders('less-loader')
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: cssLoaders('sass-loader')
+      },
+      // Loading images
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ['file-loader']
+      },
+      // Loading fonts
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: ['file-loader']
+      },
+      // Loading JS
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: jsLoaders()
+      },
+      // Loading typescript
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: {
+          loader: 'babel-loader',
+          options: babelOptions('@babel/preset-typescript')
+        }
+      }
+    ]
+  }
+};
